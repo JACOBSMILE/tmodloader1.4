52401a2d39aad602c94b19a0e168d263e11394d4
EuroHsu <telstartx2@gmail.com>
2025-04-01 10:29:51 +0800

    remove echo from autosave.sh to avoid spamming the server

--- autosave.sh
+++ autosave.sh
@@ -3,7 +3,7 @@
 while true
 do
     sleep ${TMOD_AUTOSAVE_INTERVAL}m
-    echo "[SYSTEM] Saving world..."
+    #echo "[SYSTEM] Saving world..."
     inject "save"
-    inject "say The World has been saved."
+    #inject "say The World has been saved."
 done
